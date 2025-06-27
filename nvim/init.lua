b0a211ea11ec3edb5f58794e6f8945c136b789bf
Tadeas Wilczek <tadeas.wilczek@gmail.com>
2025-06-27 15:31:55 +0200

    First usable configuration

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- Show absolute number on the current line
+vim.opt.number = true
+-- Show relative line numbers
+vim.opt.relativenumber = true
+-- Number of spaces tab counts for
+vim.opt.tabstop = 4
+-- Number of spaces for indentation
+vim.opt.shiftwidth = 4
+-- Convert tabs to spaces
+vim.opt.expandtab = true
+-- Enable smart indentation
+vim.opt.smartindent = true
